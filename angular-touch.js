1a2eb7e0a80e6b7a1161aa370dcca18d504a7532
jenkins <ci-bot@angularjs.org>
2014-04-17 09:54:23 -0700

    v1.2.17-build.116+sha.b6eb5fd

--- angular-touch.js
+++ angular-touch.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.17-build.115+sha.5bf6e50
+ * @license AngularJS v1.2.17-build.116+sha.b6eb5fd
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
