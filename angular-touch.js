164fd68535f0d4774d51c7bfcc6c9ac9f158e0c9
jenkins <ci-bot@angularjs.org>
2016-06-15 07:04:24 -0700

    v1.5.7-build.4880+sha.915e7a1

--- angular-touch.js
+++ angular-touch.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.7-build.4877+sha.8cd9848
+ * @license AngularJS v1.5.7-build.4880+sha.915e7a1
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
