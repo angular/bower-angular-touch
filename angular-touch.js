eb32e257655bed4d5af578ddfd9dda1ecce8bf54
Pete Bacon Darwin <pete@bacondarwin.com>
2018-09-07 10:29:12 +0100

    v1.7.4

--- angular-touch.js
+++ angular-touch.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.7.3
+ * @license AngularJS v1.7.4
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -29,7 +29,7 @@
 /* global ngTouch */
 var ngTouch = angular.module('ngTouch', []);
 
-ngTouch.info({ angularVersion: '1.7.3' });
+ngTouch.info({ angularVersion: '1.7.4' });
 
 function nodeName_(element) {
   return angular.$$lowercase(element.nodeName || (element[0] && element[0].nodeName));
